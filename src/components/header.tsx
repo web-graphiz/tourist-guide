695251fc4ad21e93a5e290db7a8853de7e36c00a
Geekboots <info@geekboots.com>
2024-09-05 13:22:00 +0530

    minor update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,31 +6,31 @@ const Header = () => {
       <nav className="flex gap-10 justify-center items-center">
         <Link
           href=""
-          className="uppercase font-semibold text-base text-white px-5 py-1 rounded-2xl transition-all hover:bg-green  shadow-md"
+          className="uppercase font-semibold text-base text-white px-5 py-1 rounded-2xl transition-all bg-green shadow-md"
         >
           Home
         </Link>
         <Link
           href=""
-          className="uppercase font-semibold text-base text-white px-5 py-1 rounded-2xl transition-all hover:bg-green shadow-md"
+          className="uppercase font-semibold text-base text-white px-5 py-1 rounded-2xl transition-all hover:bg-green hover:shadow-md"
         >
           About
         </Link>
         <Link
           href=""
-          className="uppercase font-semibold text-base text-white px-5 py-1 rounded-2xl transition-all hover:bg-green shadow-md"
+          className="uppercase font-semibold text-base text-white px-5 py-1 rounded-2xl transition-all hover:bg-green hover:shadow-md"
         >
           Gallery
         </Link>
         <Link
           href=""
-          className="uppercase font-semibold text-base text-white px-5 py-1 rounded-2xl transition-all hover:bg-green shadow-md"
+          className="uppercase font-semibold text-base text-white px-5 py-1 rounded-2xl transition-all hover:bg-green hover:shadow-md"
         >
           Packages
         </Link>
         <Link
           href=""
-          className="uppercase font-semibold text-base text-white px-5 py-1 hover:bg-green rounded-2xl transition-all shadow-md"
+          className="uppercase font-semibold text-base text-white px-5 py-1 hover:bg-green rounded-2xl transition-all hover:shadow-md"
         >
           Contact
         </Link>
